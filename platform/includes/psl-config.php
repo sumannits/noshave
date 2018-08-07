a87171ac4f36c52ffdc07f8175bd0aced3d77f8d
nitssubhradip <subhradip@natitsolved.com>
2018-08-07 12:57:24 +0530

    new prj setup

--- platform/includes/psl-config.php
+++ platform/includes/psl-config.php
@@ -2,9 +2,10 @@
 // USER THE DB PROXY
 
 // Using the SQL proxy
-define("HOST","localhost");
-define("USER","root");
+define("HOST","192.168.1.68");
+define("USER","shave");
 define("PASSWORD","abc123");
+define("PORT","3306");
 define("SOCKET","/cloudsql/no-shave-november:us-central1:nsn-dev");
 define("DATABASE", "no-shave");    // The database name.
 define("CAN_REGISTER", "any");
